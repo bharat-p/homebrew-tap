0c7407e2dbbb131f99939999a36fd1260327817d
releasebot <13206972+bharat-p@users.noreply.github.com>
2018-04-28 13:09:03 -0700

    Brew formula update for mongo-sync-go version v0.1.0

--- Formula/mongo-sync-go.rb
+++ Formula/mongo-sync-go.rb
@@ -3,7 +3,7 @@ class MongoSyncGo < Formula
   homepage "https://github.com/bharat-p/mongo-sync-go/blob/master/README.md"
   url "https://github.com/bharat-p/mongo-sync-go/releases/download/v0.1.0/mongo-sync-go_0.1.0_darwin_amd64.tar.gz"
   version "0.1.0"
-  sha256 "179bc90d5fe25f8b47e58019db4756cbd07ad55fcdb8c43497c15a1c5730d207"
+  sha256 "b483c8f4ed406b59c21d3551e046788a8c3385b23c0593e725fa5dd9ee004ede"
 
   def install
     bin.install "mongo-sync-go"
